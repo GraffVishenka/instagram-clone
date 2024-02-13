b0331fd824e238995e8b9ba414a195251fa1d361
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-02-13 20:00:44 +0500

    Sign up/in work

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 
+import { Toaster } from "./components/ui/toaster";
+
 import "./globals.css";
 
 const App = () => {
@@ -23,6 +25,8 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+
+      <Toaster/>
     </main>
   );
 };
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,13 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useToast } from "@/components/ui/use-toast";
+import { useUserContext } from "@/context/AuthContext";
+import {
+  useCreateUserAccount,
+  useSignInAccount,
+} from "@/lib/react-query/queriesAndMutations";
 
 import { SignupValidation } from "@/lib/validation";
 
@@ -18,7 +24,14 @@ import { Button } from "@/components/ui/button";
 import Loader from "@/components/shared/loader";
 
 const SignupForm = () => {
-  const isLoading = false;
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const {checkAuthUser, isLoading: isUserLoading} = useUserContext();
+
+  const { mutateAsync: createUserAccount, isPending: isCreatingAccount } =
+    useCreateUserAccount();
+  const { mutateAsync: signInAccount, isPending: isSigningIn } =
+    useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -34,13 +47,41 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Create User
-    //const newUser = await createUserAccount(values);
+    const newUser = await createUserAccount(values);
+
+    if (!newUser) {
+      return toast({
+        title: "Sign up failed. Please try again.",
+      });
+    }
+
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (!session) {
+      return toast({
+        title: "Sign in failed. Please try again.",
+      });
+    }
+
+    const isLoggedIn = await checkAuthUser()
+
+    if(isLoggedIn){
+      form.reset();
+      navigate("/")
+    }else{
+      return toast({
+        title: "Sign up failed. Please try again.",
+      });
+    }
   }
 
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/public/assets/images/logo.svg" />
+        <img src="/assets/images/logo.svg" />
 
         <h2 className="h3-bold md:h2-bold pt-5">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular">
@@ -108,7 +149,7 @@ const SignupForm = () => {
           />
 
           <Button type="submit" className="shad-button_primary mt-3 ">
-            {isLoading ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
@@ -119,7 +160,10 @@ const SignupForm = () => {
 
           <p className="text-small-regular text-light-2 text-center mt-2">
             Already have an account?
-            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
               Log in
             </Link>
           </p>
